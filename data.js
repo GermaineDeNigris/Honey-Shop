13b5eed7923dd8b2e993ba3679c9a505a5f2accf
Germaine DeNigris <78915379+GermaineDeNigris@users.noreply.github.com>
2023-05-01 09:54:17 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+export const menuArray = [
+    {
+        name: "Oak Honey",
+        ingredients: ["honey" , "organic" , "all natural"],
+        price: 13,
+        imgLink: "https://cdn.shopify.com/s/files/1/2537/5594/products/Maters-Oak-Honey-L-1200W_f48448ae-a2cc-47db-8a70-4024ebdccf54.jpg?v=1673953391&width=1206",
+        id: "1"
+    },
+    {
+        name: "Pink Thyme Honey",
+        ingredients: ["honey", "organic"],
+        price: 14,
+        imgLink: "https://cdn.shopify.com/s/files/1/2537/5594/products/Maters-Honey-Pink-Thyme-1200W_e33c7831-22dc-4ee1-8063-677c86a80a75.jpg?v=1673953391&width=1206",
+        id: "2"
+    },
+    {
+        name: "Borage Honey",
+        ingredients: ["honey", "local", "organic"],
+        price: 12,
+        imgLink: "https://cdn.shopify.com/s/files/1/2537/5594/products/Maters-Honey-Borage-1200W_e1a9f032-4628-42e0-b861-99ac5e431086.jpg?v=1673953391&width=1206",
+        id: "3"
+    }
+]
---
+++ index.js
@@ -0,0 +1,105 @@
+import { menuArray } from "./data.js"
+
+const $ = (id) => document.getElementById(id)
+
+const paymentContainer = $("payment-container")
+const productFeed = $("product-feed")
+const completeOrderBtn = $("complete-order-btn")
+const checkoutRequirements = $("checkout-requirement-card")
+const payBtn = $("pay-btn")
+const paymentForm = $("payment-form")
+const closingBlock = $("closing-block")
+
+const ordersArray = []
+
+document.addEventListener("click", handleClick)
+renderProducts()
+
+function handleClick(e) {
+  const { btn, remove } = e.target.dataset
+  if (btn) {
+    getOrdersArray(btn)
+  } else if (remove) {
+    removeFromOrder(remove)
+  }
+}
+
+function renderProducts() {
+  const productHTML = menuArray
+    .map(({ imgLink, name, ingredients, price, id }) => `
+        <div id="products" class="products">
+            <img class="product-img" src="${imgLink}"> 
+            <div class="product-description">
+                <h2>${name}</h2>
+                <p>${ingredients.join(", ")}</p>
+                <h3>${"$" + price}</h3>
+            </div>
+            <div>
+            <i class="fa fa-circle-plus fa-2xl icon icon-hover" data-btn ="${id}"></i>
+            </div>
+        </div>
+        <hr class="line">
+    `)
+    .join("")
+
+  productFeed.innerHTML = productHTML
+}
+
+function getOrdersArray(productId) {
+  const existingItem = ordersArray.find((order) => order.id === productId)
+  const targetItem = menuArray.find((product) => product.id === productId)
+
+  if (existingItem) {
+    existingItem.price += targetItem.price
+    existingItem.amount++
+  } else {
+    ordersArray.push({
+      name: targetItem.name,
+      price: targetItem.price,
+      amount: 1,
+      id: targetItem.id,
+    })
+  }
+  renderOrders()
+
+  paymentContainer.classList.toggle("hidden", ordersArray.length === 0)
+}
+
+function removeFromOrder(productId) {
+  const itemIndex = ordersArray.findIndex((order) => order.id === productId)
+  ordersArray.splice(itemIndex, 1)
+  renderOrders()
+  paymentContainer.classList.toggle("hidden", ordersArray.length === 0)
+}
+
+function renderOrders() {
+  let ordersHtml = ""
+  let totalPrice = 0
+
+  for (let { name, amount, id, price } of ordersArray) {
+    ordersHtml += `
+            <div class="orders-container">
+                    <h2 id="order-name">${name}</h2>
+                    <h3 class="order-amount">x${amount}</h3>
+                    <button class="remove-btn" data-remove=${id}>REMOVE</button>
+                    <h2 class="order-price">$${price}</h2>
+            </div>
+        `
+    totalPrice += price * amount
+  }
+
+  $("price-total").innerHTML = `$ ${totalPrice}`
+  $("your-orders").innerHTML = ordersHtml
+
+  paymentContainer.style.display = ordersArray.length > 0 ? "block" : "none"
+}
+
+completeOrderBtn.addEventListener("click", () => {
+  checkoutRequirements.style.display = "block"
+})
+
+payBtn.addEventListener("click", () => {
+  paymentContainer.style.display = "none"
+  checkoutRequirements.style.display = "none"
+  closingBlock.style.display = "block"
+})
